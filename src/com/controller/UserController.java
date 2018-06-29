a05f8fad3572ee63a33785d9b7c7416d0c5db09c
zhanghouxin <599768147@qq.com>
2018-06-29 19:26:43 +0800

    xiugai

--- src/com/controller/UserController.java
+++ src/com/controller/UserController.java
@@ -6,6 +6,6 @@ public class UserController {
 	public static void main(String[] args) {
 		System.out.println("adsfasdfasdfhsdfg");
 		System.out.println("sasadd");
-		
+		System.out.println("fffffffffffffffff");
 	}
 }
---
+++ src/com/controller2/BBBB.java
@@ -0,0 +1,8 @@
+package com.controller2;
+
+public class BBBB {
+
+	public static void main(String[] args) {
+		System.out.println("sadfsadf");
+	}
+}
